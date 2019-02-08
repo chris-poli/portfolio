d80e0ea31d7ed8409a09a10bf5a0ca551005dbbd
Chris Poli <aus.poli1@gmail.com>
2019-02-08 13:43:34 +1000

    Adjusted home page.

--- chris-poli-portfolio/src/Views/Home.js
+++ chris-poli-portfolio/src/Views/Home.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 import {aboutMe, photoUrl} from "../Constants/Information";
 import SideNavbar, {NavIcon} from "../Components/Navbars/SideNavbar";
 import Footer from "../Components/Common/Footer";
-
+import facebook_logo from '../Assets/Images/facebook_logo.png'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <div className='Center'>
+                <div className='Center' style={{textAlign:'center'}}>
                     <h1>Christopher Luke Poli</h1>
+                    <hr/>
                     <h3>Email: chris.luke.poli@gmail.com</h3>
-                    <h3><a href='https://www.facebook.com/98poli'>Facebook</a></h3>
+                    <h3><a href='https://www.facebook.com/98poli' target='_blank'>
+                        <img className='icon-small' src={facebook_logo}/></a></h3>
                     <img src={photoUrl}
                          className='Avatar'/>
                     <div className="panel-body">{aboutMe}</div>
